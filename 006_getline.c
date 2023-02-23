6a600eb433c3ed14c573b33042039cf0c8e5e06b
Apple <appleeyespackage@gmail.com>
2023-02-23 03:58:22 +0000

    Update 006_getline.c

--- 006_getline.c
+++ 006_getline.c
@@ -1,4 +1,4 @@
-#includ
+#include "main.h"
 
 /**
  * input_buf - buffers chained commands
@@ -41,7 +41,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 			}
 		}
 	}
-	retu
+	return (r);
 }
 
 /**
